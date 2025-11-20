fff0be06831a834f393f41ee1def47dcc97d4e29
mdomino-cs <mfdomino@wpi.edu>
2025-11-20 14:14:27 -0500

    initial commit

---
+++ myproject/settings.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+]
+
+ROOT_URLCONF = 'myproject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {},
+    },
+]
+
+WSGI_APPLICATION = 'myproject.wsgi.application'
+
+STATIC_URL = '/static/'
